a3b3935cde93a267826d9e79d96675d508d0b05d
MMachida1461] <machida951461@gmail.com>
2024-10-30 07:58:32 +0900

    showメソッドの処理を実装

--- techpit-clothes/routes/web.php
+++ techpit-clothes/routes/web.php
@@ -4,3 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
+
+Route::group('product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+
+Route::controller(ProductController::class)->group(function () {
+  Route::name('product.')->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/product/{id}', 'show')->name('show');
+  });
+});

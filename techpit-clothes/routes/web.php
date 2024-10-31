c4fc29d960911a18ba78bf3a6feaa5dd42c13ea2
MMachida1461] <machida951461@gmail.com>
2024-11-01 08:26:49 +0900

    カートに商品を入れる処理の追加

--- techpit-clothes/routes/web.php
+++ techpit-clothes/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LineItemController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 
@@ -9,5 +10,11 @@ Route::controller(ProductController::class)->group(function () {
   Route::name('product.')->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/product/{id}', 'show')->name('show');
+  });
+});
+
+Route::controller(LineItemController::class)->group(function () {
+  Route::name('line_item.')->group(function () {
+    Route::post('/line_item/create', 'create')->name('create');
   });
 });
